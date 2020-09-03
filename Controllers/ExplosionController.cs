a877c6ccc854a9f40189b2c11740a3d499cdc1e5
Byron McDaniel <byronkmcdaniel@gmail.com>
2020-09-03 18:36:30 -0400

    Explosion

---
+++ Controllers/ExplosionController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExplosionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExplosionController : ControllerBase
+    {
+        [HttpGet]
+        public string Explode( string s)
+        {
+          return string.Join("", s.Select(c => new String(c, int.Parse(c.ToString()))));
+        }
+        
+            
+        
+    }
+}
